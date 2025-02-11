de65be84b525590bd2c11e3b0b02645b48e87a1e
Anshul Rajput <anshulrajput5195@gmail.com>
2025-02-11 21:50:21 +0530

    fixed search endpoint order

--- ats_app/views.py
+++ ats_app/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Q, Count
+from django.db.models import Q
 from rest_framework import viewsets
 from ats_app.models import Candidate
 from ats_app.serializers import CandidateSerializer
@@ -12,17 +12,32 @@ class CandidateViewSet(viewsets.ModelViewSet):
     lookup_field = "unique_id"
     serializer_class = CandidateSerializer
 
+    @staticmethod
+    def word_match(candidate, query_words):
+        candidate_words = candidate.name.split()
+        total = 0
+        for word in candidate_words:
+            if word in query_words:
+                total += 1
+        return total
+
     def get_queryset(self):
-        queryset = Candidate.objects.all()
         query_param = self.request.query_params.get("name")
         if not query_param:
-            return queryset
+            return Candidate.objects.all()
         else:
             query_words = query_param.split()
             search_conditions = Q()
+
             for word in query_words:
                 search_conditions |= Q(name__icontains=word)
-            candidates = queryset.filter(search_conditions).annotate(
-                relevance=Count('name', filter=Q(name__icontains=query_words[0]))
-            ).order_by('-relevance')
-            return candidates
+
+            candidates = list(Candidate.objects.filter(search_conditions))
+
+            candidates_word_count = {}
+            for candidate in candidates:
+                word_matches = self.word_match(candidate, query_words)
+                candidates_word_count[candidate] = word_matches
+
+            sorted_candidates = [key for key, value in sorted(candidates_word_count.items(), key=lambda item: item[1], reverse=True)]
+            return sorted_candidates
