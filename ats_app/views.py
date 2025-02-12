a851459927477d6d8d0a10f3f7d02d237d6126c3
Anshul Rajput <anshulrajput5195@gmail.com>
2025-02-12 12:07:49 +0530

    switched search query from python logic to orm logic

--- ats_app/views.py
+++ ats_app/views.py
@@ -1,4 +1,5 @@
-from django.db.models import Q
+from django.db.models import Q, Value
+from django.db.models.functions import Length, Replace
 from rest_framework import viewsets
 from ats_app.models import Candidate
 from ats_app.serializers import CandidateSerializer
@@ -12,15 +13,6 @@ class CandidateViewSet(viewsets.ModelViewSet):
     lookup_field = "unique_id"
     serializer_class = CandidateSerializer
 
-    @staticmethod
-    def word_match(candidate, query_words):
-        candidate_words = candidate.name.split()
-        total = 0
-        for word in candidate_words:
-            if word in query_words:
-                total += 1
-        return total
-
     def get_queryset(self):
         query_param = self.request.query_params.get("name")
         if not query_param:
@@ -28,16 +20,14 @@ class CandidateViewSet(viewsets.ModelViewSet):
         else:
             query_words = query_param.split()
             search_conditions = Q()
-
             for word in query_words:
                 search_conditions |= Q(name__icontains=word)
 
-            candidates = list(Candidate.objects.filter(search_conditions))
-
-            candidates_word_count = {}
-            for candidate in candidates:
-                word_matches = self.word_match(candidate, query_words)
-                candidates_word_count[candidate] = word_matches
-
-            sorted_candidates = [key for key, value in sorted(candidates_word_count.items(), key=lambda item: item[1], reverse=True)]
-            return sorted_candidates
+            # Annotate candidates with word match count
+            candidates = Candidate.objects.filter(search_conditions).annotate(
+                relevance=sum(
+                    (Length('name') - Length(Replace(expression='name', text=Value(word), replacement=Value('')))) / Length(Value(word))
+                    for word in query_words
+                )
+            ).order_by('-relevance')
+            return candidates
